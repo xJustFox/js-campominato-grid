dcf62ad701ef00ac1201ace498c89dbd7b79634f
xJustFox <dodydaversa@gmail.com>
2023-11-29 14:36:43 +0100

    [Upgrade] Exercise completed

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// FUNZIONE CHE CREA L'ELEMENTO <div>  CON CLASSE "cell"
+function createCell() {
+    const element = document.createElement("div");
+    element.classList.add("cell");
+
+    return element;
+}
+
+// BOTTONE "Start Game"
+const startButton = document.getElementById("startButton");
+
+// ISTRUZIONE BOTTONE "Start Game"
+startButton.addEventListener("click", function () {
+    const grid = document.getElementById("grid");
+    
+    // loop che genera i quadrati e gli inserisce nelle griglia
+    for (let i = 1; i <= 100; i++) {
+        let cell = createCell(i);
+
+        cell.innerText = i;
+    
+        grid.appendChild(cell);
+    }
+})
